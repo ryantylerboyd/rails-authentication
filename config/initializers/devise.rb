b7edb0a6d13dea418e9a12fce2c0f28e3191ca2a
ryan boyd <ryantylerboyd@outlook.com>
2022-11-26 16:39:00 +1100

    added the default mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,13 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # config/initializers/devise.rb
+
+  # [...]
+  # ==> Mailer Configuration
+  # Configure the e-mail address which will be shown in Devise::Mailer,
+  # note that it will be overwritten if you use your own mailer class
+  # with default "from" parameter.
+  config.mailer_sender = 'nighthawk@ryanboyd.info'
 end
